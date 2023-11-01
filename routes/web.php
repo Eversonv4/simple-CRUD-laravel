3ec690a60d6ae1ac4b6e583f8ee0503065374daf
Everson Soares <eversonv4@gmail.com>
2023-11-01 03:55:44 -0300

    project with simple CRUD of posts

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get("/posts/create", [PostController::class, "create"]);
+Route::get("/posts/read", [PostController::class, "read"]);
+Route::get("/posts/read_all", [PostController::class, "read_all"]);
+Route::get("/posts/update", [PostController::class, "update"]);
+Route::get("/posts/delete", [PostController::class, "delete"]);
